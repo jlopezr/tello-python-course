bee89ea846ec2a5a3c77706e8fbea7a0f695fa71
Juan López Rubio <j.lopez.r@gmail.com>
2022-06-22 15:25:52 +0200

    updated project 2

--- project2-mapping.py
+++ project2-mapping.py
@@ -1,28 +1,36 @@
-import math
-from time import sleep
+from djitellopy import tello
 import keypress as kp
 import numpy as np
+from time import sleep
 import cv2
-from djitellopy import tello
-import time
+import math
 
-######## PARAMETERS ##########
-fSpeed = 117/10 # forward speed in cm/s
-aSpeed = 360/10 # angular speed degrees/s
+######## PARAMETERS ###########
+fSpeed = 117 / 10  # Forward Speed in cm/s   (15cm/s)
+aSpeed = 360 / 10  # Angular Speed Degrees/s  (50d/s)
 interval = 0.25
-
 dInterval = fSpeed * interval
 aInterval = aSpeed * interval
-#############################
+###############################################
+
 x, y = 500, 500
+a = 0
 yaw = 0
 
-def getKeyboard():
+kp.init()
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+points = [(0, 0), (0, 0)]
+
+def getKeyboardInput():
     lr, fb, ud, yv = 0, 0, 0, 0
-    speed = 50
+    speed = 15
+    aspeed = 50
+
+    global x, y, yaw, a
     d = 0
-    a = 0
-    global x, y, yaw
 
     if kp.getKey("LEFT"):
         lr = -speed
@@ -48,40 +56,41 @@ def getKeyboard():
         ud = -speed
 
     if kp.getKey("a"):
-        yv = -speed
-        yaw = yaw + aInterval
+        yv = -aspeed
+        yaw -= aInterval
     elif kp.getKey("d"):
-        yv = speed
-        yaw = yaw - aInterval
+        yv = aspeed
+        yaw += aInterval
 
-    if kp.getKey("q"): me.land(); time.sleep(3) #TODO Para que necesita estar parado 3 segundos?
+    if kp.getKey("q"): me.land(); sleep(3)
+
     if kp.getKey("e"): me.takeoff()
 
     sleep(interval)
-    a = a + yaw
+
+    a += yaw
     x += int(d * math.cos(math.radians(a)))
     y += int(d * math.sin(math.radians(a)))
 
-    print((x,y,a))
-
     return [lr, fb, ud, yv, x, y]
 
+
 def drawPoints(img, points):
-    cv2.circle(img, (points[0], points[1]), 5, (0, 0, 255), cv2.FILLED)
+    for point in points:
+        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)
 
-kp.init()
-me = tello.Tello()
-me.connect()
-
-print(me.get_battery())
-
-img = np.zeros((1000, 1000, 3), np.uint8)
+    cv2.circle(img, points[-1], 8, (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'({(points[-1][0] - 500) / 100},{(points[-1][1] - 500) / 100})m',
+                (points[-1][0] + 10, points[-1][1] + 30), cv2.FONT_HERSHEY_PLAIN, 1,
+                (255, 0, 255), 1)
 
 while True:
-    values = getKeyboard()
-    me.send_rc_control(values[0], values[1], values[2], values[3])
+    vals = getKeyboardInput()
+    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    img = np.zeros((1000, 1000, 3), np.uint8)
+    if points[-1][0] != vals[4] or points[-1][1] != vals[5]:
+        points.append((vals[4], vals[5]))
+    drawPoints(img, points)
 
-    points = (values[4], values[5])
-    drawPoints(img,points)
     cv2.imshow("Output", img)
     cv2.waitKey(1)
